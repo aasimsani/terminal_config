d0c89e4b8399b63f9c8cc9183d8e56d3805f1e1e
aasimsani <aasimsani05@gmail.com>
2018-02-04 22:31:48 +0000

    Added other configurations and a setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,15 @@
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+apt-get install tmux
+if [ -f ~/.bashrc ];
+    then
+        echo ".bashrc exists. Appending"
+        echo 'set -o vi' >> ~/.bashrc
+else
+        echo ".bashrc doesn't exist. Creating then appending"
+        touch ~/.bashrc
+        echo 'set -o vi' >> ~/.bashrc
+fi
 
 cp ./.vimrc ~/
