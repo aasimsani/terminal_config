617d0ec79458b439fbb6e48e08973d40fb7a8e0d
Aasim Sani <aasimsani05@gmail.com>
2018-02-04 19:20:32 -0500

    Open NERDTree with <Space>

--- .vimrc
+++ .vimrc
@@ -51,3 +51,8 @@ nnoremap gk <C-W><C-K>
 nnoremap gl <C-W><C-L> 
 
 set number
+map <Space> :NERDTreeToggle<CR>
+autocmd StdinReadPre * let s:std_in=1
+autocmd VimEnter * if argc() == 1 && isdirectory(argv()[0]) && !exists("s:std_in") | exe 'NERDTree' argv()[0] | wincmd p | ene | endif
+autocmd bufenter * if (winnr("$") == 1 && exists("b:NERDTree") && b:NERDTree.isTabTree()) | q | endif
+
--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 apt-get install tmux
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 if [ -f ~/.bashrc ];
     then
         echo ".bashrc exists. Appending"
